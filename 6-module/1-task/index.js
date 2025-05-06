565281468473dfd3b130be7946002d3acbe61ecf
69IstominaEA <69IstominaEA@region.cbr.ru>
2025-05-06 19:21:26 +0300

    Task 6.1 добавлен класс UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,51 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = rows;
+  }
+
+  get elem() {
+    return this._elem;
+  }
+
+  set elem(rows) {
+    let root = document.createElement('table');
+    let html = `
+      <thead>
+        <tr>
+          <td>Имя</td>
+          <td>Возраст</td>
+          <td>Зарплата</td>
+          <td>Город</td>
+          <td></td>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    `;
+    root.insertAdjacentHTML('afterbegin', html);
+
+    let body = root.querySelector('tbody');
+    rows.forEach((item) => {
+      let rowHtml = `
+        <tr>
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.salary}</td>
+          <td>${item.city}</td>
+          <td><button>X</button></td>
+        </tr>
+      `;
+      body.insertAdjacentHTML('beforeend', rowHtml);
+    })
+    root.onclick = function(event){
+      let target = event.target; // где клик?
+
+      if (target.tagName != 'BUTTON') return;
+
+      let tr = event.target.closest('tr');
+
+      tr.remove();
+    }
+    this._elem = root;
   }
 }
